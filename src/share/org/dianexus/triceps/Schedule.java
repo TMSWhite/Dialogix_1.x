003d09165d731d6155b86a1f49623992d08f3173
twhite_cvs <>
2000-05-24 15:50:10 +0000

    improved reserved words functionality; added a few more

--- src/share/org/dianexus/triceps/Schedule.java
+++ src/share/org/dianexus/triceps/Schedule.java
@@ -7,27 +7,49 @@ import java.net.*;
  * Schedule holds a collection of nodes.
 */
 public class Schedule  {
-	private static final int TITLE = 0;
-	private static final int STARTING_STEP = 1;
-	private static final int START_TIME = 2;
-	private static final int PASSWORD_FOR_REFUSED = 3;
-	private static final int AUTOGEN_OPTION_NUM = 4;
+	public static final int TITLE = 0;
+	public static final int STARTING_STEP = 1;
+	public static final int START_TIME = 2;
+	public static final int PASSWORD_FOR_REFUSED = 3;
+	public static final int AUTOGEN_OPTION_NUM = 4;
+	public static final int FILENAME = 5;
+	public static final int PASSWORD_FOR_UNKNOWN = 6;
+	public static final int ICON = 7;
+	public static final int HEADER_MSG = 8;
 	
-	private static final String[] RESERVED_WORDS = {
-		"__TITLE__", "__STARTING_STEP__", "__START_TIME__", "__PASSWORD_FOR_REFUSED__", "__AUTOGEN_OPTION_NUM__"
+	public static final String[] RESERVED_WORDS = {
+		"__TITLE__", "__STARTING_STEP__", "__START_TIME__", "__PASSWORD_FOR_REFUSED__", "__AUTOGEN_OPTION_NUM__", "__FILENAME__",
+		"__PASSWORD_FOR_UNKNOWN__", "__ICON__", "__HEADER_MSG__"
 	};
 	
-	private String title = "Triceps System";
-	private Integer startingStep = new Integer(0);
+	private String title = null;
+	private Integer startingStep = null;
 	private Date startTime = null;
-	private String passwordForRefused = null;	// disabled by default
+	private String passwordForRefused = null;
+	private String filename = null;
+	private String passwordForUnknown = null;
+	private String icon = null;
+	private String headerMsg = null;
+	
 	
 	private Vector nodes = new Vector();
 	private Vector comments = new Vector();
 	private Hashtable reserved = new Hashtable();
 
 	public Schedule() {
-		startTime = new Date(System.currentTimeMillis());
+		setDefaultReserveds();
+	}
+	
+	public void setDefaultReserveds() {
+		setReserved(TITLE,"Triceps System");
+		setReserved(STARTING_STEP,"0");
+		setReserved(START_TIME,Datum.TIME_MASK.format(new Date(System.currentTimeMillis())));
+		setReserved(PASSWORD_FOR_REFUSED,"");
+		setReserved(AUTOGEN_OPTION_NUM,"true");
+		setReserved(FILENAME,getReserved(START_TIME));
+		setReserved(PASSWORD_FOR_UNKNOWN,"");
+		setReserved(ICON,"");
+		setReserved(HEADER_MSG,"Triceps System");
 	}
 
 	public boolean load(BufferedReader br, String filename) {
@@ -43,8 +65,7 @@ public class Schedule  {
 
 			while ((fileLine = br.readLine()) != null) {
 				++line;
-				fileLine = fileLine.trim();
-				if (fileLine.equals(""))
+				if (fileLine.trim().equals(""))
 					continue;
 					
 				if (fileLine.startsWith("COMMENT")) {
@@ -71,7 +92,7 @@ public class Schedule  {
 			if (br != null) {
 				try { br.close(); } catch (Exception e) {}
 			}
-			setStartTime(Datum.TIME_MASK.format(startTime));
+			setStartTime(startTime);
 		}
 	}
 
@@ -131,39 +152,72 @@ public class Schedule  {
 			}
 		}
 		
-		switch (resIdx) {
-			case TITLE: setTitle(value); break;
-			case STARTING_STEP: setStartingStep(value); break;
-			case START_TIME: setStartTime(value); break;
-			case PASSWORD_FOR_REFUSED: setPasswordForRefused(value); break;
-			case AUTOGEN_OPTION_NUM: setAutoGenOptionNum(value); break;
-			default: System.out.println("unrecognized reserved word " + name + " on line " + line + " of file " + filename); break;
+		if (!setReserved(resIdx, value)) {
+			System.out.println("unrecognized reserved word " + name + " on line " + line + " of file " + filename); 
 		}
 	}
+		
+	public boolean setReserved(int resIdx, String value) {
+		String s;
+		switch (resIdx) {
+			case TITLE: s = setTitle(value); break;
+			case STARTING_STEP: s = setStartingStep(value); break;
+			case START_TIME: s = setStartTime(value); break;
+			case PASSWORD_FOR_REFUSED: s = setPasswordForRefused(value); break;
+			case AUTOGEN_OPTION_NUM: s = setAutoGenOptionNum(value); break;
+			case FILENAME: s = setFilename(value); break;
+			case PASSWORD_FOR_UNKNOWN: s = setPasswordForUnknown(value); break;
+			case ICON: s = setIcon(value); break;
+			case HEADER_MSG: s = setHeaderMsg(value); break;
+			default: return false;
+		}
+		reserved.put(RESERVED_WORDS[resIdx], s);
+		return true;
+	}
 	
-	public void setAutoGenOptionNum(String s) {
+	public String getReserved(int resIdx) {
+		if (resIdx >= 0 && resIdx < RESERVED_WORDS.length) {
+			return (String) reserved.get(RESERVED_WORDS[resIdx]);
+		}
+		else
+			return null;
+	}
+		
+	private String setAutoGenOptionNum(String s) {
 		Boolean b = Boolean.valueOf(s);
 		Node.setAutoGenOptionAccelerator(b.booleanValue());	// very much of a hack - call Node directly
-		reserved.put(RESERVED_WORDS[AUTOGEN_OPTION_NUM], b.toString());
+		return b.toString();
 	}
 	
-	public void setPasswordForRefused(String s) { 
+	private String setPasswordForRefused(String s) { 
 		passwordForRefused = s; 
-		reserved.put(RESERVED_WORDS[PASSWORD_FOR_REFUSED],s);
+		return s;
 	}
-	public String getPasswordForRefused() { return passwordForRefused; }
+	private String setPasswordForUnknown(String s) { 
+		passwordForUnknown = s; 
+		return s;
+	}
 	
-	public void setTitle(String s) { 
+	private String setIcon(String s) {
+		icon = s;
+		return s;
+	}
+	
+	private String setHeaderMsg(String s) {
+		headerMsg = s;
+		return s;
+	}
+	
+	private String setTitle(String s) { 
 		if (s == null)
 			title = "";
 		else
 			title = s;
 			
-		reserved.put(RESERVED_WORDS[TITLE],title);
+		return title;
 	}
-	public String getTitle() { return title; }
 	
-	public void setStartingStep(String s) { 
+	private String setStartingStep(String s) { 
 		try {
 			startingStep = new Integer(s);
 		}
@@ -171,11 +225,17 @@ public class Schedule  {
 			System.out.println(e);
 			startingStep = new Integer(0);
 		}
-		reserved.put(RESERVED_WORDS[STARTING_STEP],startingStep);
+		return startingStep.toString();
 	}
-	public int getStartingStep() { return startingStep.intValue(); }
 	
-	public void setStartTime(String t) {
+	private String setStartTime(Date t) {
+		startTime = t;
+		String str = Datum.TIME_MASK.format(startTime);
+		reserved.put(RESERVED_WORDS[START_TIME], str);
+		return str;
+	}
+	
+	private String setStartTime(String t) {
 		Date time = null;
 		try {
 			time = Datum.TIME_MASK.parse(t);
@@ -183,16 +243,17 @@ public class Schedule  {
 		catch (java.text.ParseException e) {
 			System.out.println("Error parsing time " + e.getMessage());
 		}
-		if (time != null) {
-			startTime = time;
-			reserved.put(RESERVED_WORDS[START_TIME],t);
+		if (time == null) {
+			time = new Date(System.currentTimeMillis());
 		}
-		else {
-			startTime = new Date(System.currentTimeMillis());
-			reserved.put(RESERVED_WORDS[START_TIME],Datum.TIME_MASK.format(startTime));
-		}
+		return setStartTime(time);
 	}
-	public Date getStartTime() { return startTime; }
+	
+	private String setFilename(String value) {
+		filename = value;
+		return filename;
+	}
+	
 	
 	public void toTSV(Writer out) {
 		Enumeration keys = reserved.keys();
