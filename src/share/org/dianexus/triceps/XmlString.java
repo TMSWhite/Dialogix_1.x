afa89966b6c6e5df7e864f68322025f757fadea7
twhite_cvs <>
2000-06-08 18:20:14 +0000

    now able to deal with any HTML tag between the <body> tags. the dangerous tags (applet, embed, object, script), layer, and frame tags are disabled <!-- comments are disabled -->

--- src/share/org/dianexus/triceps/XmlString.java
+++ src/share/org/dianexus/triceps/XmlString.java
@@ -4,7 +4,23 @@ public class XmlString extends Object {
 	private static Hashtable HTMLentities = new Hashtable();
 	private static Hashtable HTMLtags = new Hashtable();
 	private static String allowableHTMLtags[] = {
-		"b","/b","i","/i","u","/u","br","hr",	
+		"a","/a","abbr","/abbr","acronym","/acronym","address","/address",	/*"applet","/applet",*/ "area", 
+		"b","/b","base","basefont","bdo","/bdo", /*"bgsound",*/ "big","/big", /*"blink","/blink",*/ "blockquote","/blockquote", /*"body","/body",*/ "br","button",
+		"caption","/caption","center","/center","cite","/cite","code","/code","col","colgroup","comment","/comment",
+		"dd","/dd","del","/del","dfn","/dfn","dir","/dir","div","/div","dl","/dl","dt","/dt",
+		"em","/em", /*"embed","/embed",*/ "fieldset","/fieldset","font","/font","form","/form", /*"frame","/frame","frameset","/frameset"*/
+		"h1","/h1","h2","/h2","h3","/h3","h4","/h4","h5","/h5","h6","/h6", /*"head","/head",*/ "hr", /*"html","/html",*/
+		"i","/i", /*"iframe","/iframe","ilayer","/ilayer",*/ "img","input","ins","/ins", /*"isindex",*/
+		"kbd","/kbd", /*"keygen","/keygen",*/
+		"label","/label", /*"layer","/layer",*/ "legend","/legend","li","/li","link","listing","/listing",
+		"map","/map", /*"marquee","/marquee",*/ "menu","/menu", /*"meta","/meta","multicol","/multicol",*/
+		"nobr","/nobr", /*"noembed","/noembed","noframes","/noframes","noscript","/noscript",*/
+		/*"object","/object",*/ "ol","/ol","optgroup","/optgroup","option","/option",
+		"p","/p", /*"param","/param"*/ "plaintext","pre","/pre",
+		"q","/q",
+		"s","/s", /*"script","/script"*/ "select","/select", /*"server","/server",*/ "small","/small", /*"spacer",*/ "span","/span","strike","/strike","strong","/strong", /*"style","style",*/ "sub","/sub","sup","/sup",
+		"table","/table","tbody","/tbody","td","/td","textarea","/textarea","tfoot","/tfoot","th","/th","thead","/thead", /*"title","/title",*/ "tr","/tr","tt","/tt",
+		"ul","/ul","var","/var","wbr", /*"xmp","/xmp"*/		
 	};
 	private static String allowableHTMLentities[] = {
 		"&quot;","&amp;","&lt;","&gt;","&nbsp;","&iexcl;","&cent;","&pound;","&curren;",
@@ -42,26 +58,80 @@ public class XmlString extends Object {
     
     public	String toString() { return value; }
     
+    private static final int NMTOKEN = 0;
+    private static final int EQUALS = 1;
+    private static final int START_STRING = 2;
+    private static final int END_STRING = 3;
+    private static final String[] parsingPosition = { "NMTOKEN","EQUALS","STARTING_STRING","END_STRING" };
+    
 	private boolean isValidElement(String element) {
-		String token;
+		String token = null;
+		String quoteChar = null;
+		boolean withinEscape = false;
 		
 		try {
 			/* build a string stripped of the opening and closing angle brackets */
-			StringTokenizer st = new StringTokenizer(element.substring(1,element.length()-1)," \t\n\r\f=\'\"",true);
+			StringTokenizer st = new StringTokenizer(element.substring(1,element.length()-1)," \t\n\r\f=\'\"\\",true);
 				
+			/* get and check tag name */
 			token = st.nextToken().toLowerCase();
 			if (!HTMLtags.containsKey(token))
 				return false;
-				
+	
+			int which = NMTOKEN;				
 			while (st.hasMoreTokens()) {
+				/* Loop over name = "attribute" pairs */				
 				token = st.nextToken();
-				
-				if (" ".equals(token) || "\t".equals(token) || "\n".equals(token) || "\r".equals(token) || "\f".equals(token))
-					continue;	// ignore whitespace
-				
-				/* should now loop over name = "attribute" pairs */
+				if (Character.isWhitespace(token.charAt(0)))
+					continue;
+//				if (" ".equals(token) || "\t".equals(token) || "\n".equals(token) || "\r".equals(token) || "\f".equals(token))
+						
+				switch(which) {
+					case NMTOKEN: {
+						char[] chars = token.toCharArray();
+						for (int i=0;i<chars.length;++i) {
+							if (!(Character.isLetterOrDigit(chars[i]) || chars[i] == '_')) {
+//System.err.println("bad character in NMTOKEN: " + chars[i]);								
+								return false;
+							}
+						}
+						which = EQUALS;
+					}
+						break;
+					case EQUALS:
+						if (!"=".equals(token)) {
+//System.err.println("expected equals sign, got: " + token);							
+							return false;
+						}
+						which = START_STRING;
+						break;
+					case START_STRING: 
+						quoteChar = token;
+						if (!("\"".equals(quoteChar) || "\'".equals(quoteChar))) {
+//System.err.println("expected the start of a string, got: " + token);							
+							return false;
+						}
+						withinEscape = false;
+						which = END_STRING;
+						break;
+					case END_STRING:
+						if ("\\".equals(token)) {
+							withinEscape = !(withinEscape);
+							continue;
+						}
+						if (quoteChar.equals(token) && !withinEscape) {
+							/* normal end of that string */
+							which = NMTOKEN;
+						}
+						break;
+				}
 			}
-			return true;
+			if (which == NMTOKEN)
+				return true;
+			else {
+//System.err.println("prematurely terminated attribute-value pair - ended expecting: " + parsingPosition[which]	);		
+				return false;	// unterminated attribute-value pairs
+			}
 		}
 		catch (Throwable t) {
 			return false;	// invalid element syntax
