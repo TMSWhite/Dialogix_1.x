7db7a01f781b1cc9f7e3d7df55c44f1f878ada94
twhite_cvs <>
2000-06-13 20:48:17 +0000

    added more error checking options

--- src/share/org/dianexus/triceps/ParseError.java
+++ src/share/org/dianexus/triceps/ParseError.java
@@ -4,22 +4,39 @@ import java.io.*;
 
 
 public class ParseError  {
-	Node node;
-	String dependenciesErrors;
-	String actionErrors;
-	Vector nodeErrors;
+	Node node = null;
+	String dependenciesErrors = null;
+	String actionErrors = null;
+	String answerChoicesErrors = null;
+	String readbackErrors = null;
+	Vector nodeParseErrors = null;
+	Vector nodeNamingErrors = null;
 
-	public ParseError(Node node, String d, String a, Vector n) {
+	public ParseError(Node node, String dependenciesErrors, String actionErrors, String answerChoicesErrors, String readbackErrors, Vector nodeParseErrors, Vector nodeNamingErrors) {
 		this.node = node;
-		dependenciesErrors = d;
-		actionErrors = a;
-		nodeErrors = n;
+		this.dependenciesErrors = dependenciesErrors;
+		this.actionErrors = actionErrors;
+		this.answerChoicesErrors = answerChoicesErrors;
+		this.readbackErrors = readbackErrors;
+		this.nodeParseErrors = nodeParseErrors;
+		this.nodeNamingErrors = nodeNamingErrors;
+		
 	}
 
-	public String getDependencies() { return node.getDependencies(); }
 	public String getDependenciesErrors() { return dependenciesErrors; }
-	public String getQuestionOrEval() { return node.getQuestionOrEval(); }
 	public String getQuestionOrEvalErrors() { return actionErrors; }
-	public Vector getNodeErrors() { return nodeErrors; }
+	public String getAnswerChoicesErrors() { return answerChoicesErrors; }
+	public String getReadbackErrors() { return readbackErrors; }
+	public Vector getNodeParseErrors() { return nodeParseErrors; }
+	public Vector getNodeNamingErrors() { return nodeNamingErrors; }
+	
 	public Node getNode() { return node; }
+	
+	public boolean hasDependenciesErrors() { return (dependenciesErrors != null && dependenciesErrors.length() > 0); }
+	public boolean hasQuestionOrEvalErrors() { return (actionErrors != null && actionErrors.length() > 0); }
+	public boolean hasAnswerChoicesErrors() { return (answerChoicesErrors != null && answerChoicesErrors.length() > 0); }
+	public boolean hasReadbackErrors() { return (readbackErrors != null && readbackErrors.length() > 0); }
+	public boolean hasNodeParseErrors() { return (nodeParseErrors != null && nodeParseErrors.size() > 0); }
+	public boolean hasNodeNamingErrors() { return (nodeNamingErrors != null && nodeNamingErrors.size() > 0); }
+	
 }
