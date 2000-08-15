0d8f5c7fbbc3c256195abe8fdada458d455a6b81
twhite_cvs <>
2000-08-15 17:39:43 +0000

    Differences between 1.3.6 and 1.3.5 General   Implemented version control via VersionIF   Release binaries can have differnet modules optionally compiled using AUTHORABLE and DEBUG   Now jarred into a package:  org.dianexus.triceps.*; Functions   trim() Schedule   ACTIVE_BUTTON_PREFIX   ACTIVE_BUTTON_SUFFIX   HELP_ICON   implemented functionality for COMMENT_ICON_ON, etc ICON references User Interface   button_prefix / suffix used to dynamically indicate which button is active BugFixes   event recording for combo-box navigation improved.

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -1,9 +1,9 @@
 package org.dianexus.triceps;
 
 public interface VersionIF {
-    public final static boolean DEBUG = false;
-    public final static boolean AUTHORABLE = false;
-    public final static String VERSION_MAJOR = "2.0";
-    public final static String VERSION_MINOR = "0";
+    public final static boolean DEBUG = true;
+    public final static boolean AUTHORABLE = true;
+    public final static String VERSION_MAJOR = "1.3";
+    public final static String VERSION_MINOR = "6";
     public final static String VERSION_NAME = "Triceps version " + VERSION_MAJOR + "." + VERSION_MINOR;
 }
