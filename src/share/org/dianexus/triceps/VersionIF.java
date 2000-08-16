ed5406613466684057d80467cc781fa0a7a68d7b
twhite_cvs <>
2000-08-16 21:06:06 +0000

    updated to version 1.3.7

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -4,6 +4,6 @@ public interface VersionIF {
     public final static boolean DEBUG = true;
     public final static boolean AUTHORABLE = true;
     public final static String VERSION_MAJOR = "1.3";
-    public final static String VERSION_MINOR = "6";
+    public final static String VERSION_MINOR = "7";
     public final static String VERSION_NAME = "Triceps version " + VERSION_MAJOR + "." + VERSION_MINOR;
 }
