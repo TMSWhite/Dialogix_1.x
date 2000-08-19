7c7b6b4d45e535af3faab8ed77bb154bfffa5e7f
twhite_cvs <>
2000-08-19 16:51:43 +0000

    added VERSION_TYPE, and updated VERSION_NAME so shows whether Authorable or Deployable or both

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -3,7 +3,9 @@ package org.dianexus.triceps;
 public interface VersionIF {
     public final static boolean DEBUG = true;
     public final static boolean AUTHORABLE = true;
-    public final static String VERSION_MAJOR = "1.3";
-    public final static String VERSION_MINOR = "7";
-    public final static String VERSION_NAME = "Triceps version " + VERSION_MAJOR + "." + VERSION_MINOR;
+    public final static boolean DEPLOYABLE = true;
+    public final static String VERSION_MAJOR = "1.4";
+    public final static String VERSION_MINOR = "0";
+    public final static String VERSION_TYPE = ((AUTHORABLE && DEPLOYABLE) ? "Development System" : ((AUTHORABLE) ? "Authoring System" : ((DEPLOYABLE) ? "Interviewing System" : "Demo")));
+    public final static String VERSION_NAME = "Triceps " + VERSION_TYPE + " version " + VERSION_MAJOR + "." + VERSION_MINOR;
 }
