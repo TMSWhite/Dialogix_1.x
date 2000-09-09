2a88822d30bda1c2e90303328a7d746a591742dd
twhite_cvs <>
2000-09-09 16:47:46 +0000

    alpha version of 2.1.0

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -4,10 +4,10 @@ import java.lang.String;
 
 /*public*/ interface VersionIF {
     /*public*/ final static boolean DEBUG = true;
-    /*public*/ final static boolean AUTHORABLE = false;
+    /*public*/ final static boolean AUTHORABLE = true;
     /*public*/ final static boolean DEPLOYABLE = true;
-    /*public*/ final static String VERSION_MAJOR = "2.0";
-    /*public*/ final static String VERSION_MINOR = "1";
+    /*public*/ final static String VERSION_MAJOR = "2.1";
+    /*public*/ final static String VERSION_MINOR = "0a";
     /*public*/ final static String VERSION_TYPE = ((AUTHORABLE && DEPLOYABLE) ? "Development System" : ((AUTHORABLE) ? "Authoring System" : ((DEPLOYABLE) ? "Interviewing System" : "Demo")));
     /*public*/ final static String VERSION_NAME = "Triceps " + VERSION_TYPE + " version " + VERSION_MAJOR + "." + VERSION_MINOR;
 }
