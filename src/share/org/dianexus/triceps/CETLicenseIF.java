78d67268f38c11a08145b132a95294a308906a58
twhite_cvs <>
2001-06-13 21:55:52 +0000

    new license file

---
+++ src/share/org/dianexus/triceps/CETLicenseIF.java
@@ -0,0 +1,17 @@
+/* ******************************************************** 
+** Copyright (c) 2000-2001, Thomas Maxwell White, all rights reserved. 
+** $Header$
+******************************************************** */ 
+
+package org.dianexus.triceps;
+
+import java.lang.String;
+
+interface CETLicenseIF  {
+	final static String STUDY_NAME = "Center for Environmental Therapeutics";
+	final static String STUDY_ALIAS = "CET";
+	final static String STUDY_ICON = "/images/CETbulblogo.jpg";
+	final static String PRINCIPAL_INVESTIGATOR = "Michael Terman, Ph.D.";
+	final static String GRANT_NAME = "NIMH Grant MH42931";
+	final static String GRANT_TITLE = "Light and Ion Therapy for Seasonal Affective Disorder";
+}
