46d7069d0e83b6acc79f5d5d5570bc920a5b6bd4
twhite_cvs <>
2001-06-14 01:49:29 +0000

    if .jar files are found in /working directory, unzips them (warning if a larger file already exists), then deletes them

--- src/share/org/dianexus/triceps/ScheduleList.java
+++ src/share/org/dianexus/triceps/ScheduleList.java
@@ -9,6 +9,12 @@ package org.dianexus.triceps;
 /*import java.util.*;*/
 import java.util.Vector;
 import java.io.File;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.util.jar.JarFile;
+import java.util.jar.JarEntry;
+import java.util.Enumeration;
 
 /*public*/ final class ScheduleList implements VersionIF {
 	private Vector schedules = new Vector();
@@ -16,7 +22,7 @@ import java.io.File;
 	private Logger logger = new Logger();
 	private Triceps triceps = Triceps.NULL;
 
-    /*public*/ ScheduleList(Triceps lang, String sourceDir) {
+    /*public*/ ScheduleList(Triceps lang, String sourceDir, boolean isSuspended) {
      	triceps = (lang == null) ? Triceps.NULL : lang;
 	   	this.sourceDir = sourceDir;
 	    File dir = new File(sourceDir);
@@ -28,6 +34,10 @@ import java.io.File;
 	    else if (!dir.canRead()) {
 	    	setError(sourceDir + triceps.get("is_not_accessible"));
 	    	return;
+	    }
+	    
+	    if (isSuspended) {
+	    	unjarSuspendedInterviews(dir);
 	    }
 
 		String[] files = dir.list();
@@ -48,6 +58,93 @@ import java.io.File;
 		}
 		triceps.setLocale(null);	// set it to the default
     }
+    
+    private void unjarSuspendedInterviews(File dir) {
+		String[] files = dir.list();
+
+		int count=0;
+		for (int i=0;i<files.length;++i) {
+			File f = new File(dir.toString() + "/" + files[i]);
+			try {
+				if (f.getName().endsWith(".jar") && f.canRead()) {
+					// unjar file and remove it
+					unjar(f);
+					f.delete();	// delete regardless of whether successfully unjared
+				}
+			}
+			catch (Exception e) {
+				setError("unjarSuspendedInterviews: " + e.getMessage());
+			}				
+		}    	
+    }
+    
+    private boolean unjar(File file) {
+		JarFile jf = null;
+		boolean ok = true;
+		
+		try {
+			jf = new JarFile(file);
+			Enumeration entries = jf.entries();
+			
+			while(entries.hasMoreElements()) {
+				if (!saveUnjaredFile(jf, (JarEntry) entries.nextElement())) {
+					ok = false;
+					break;
+				}
+			}
+		}
+		catch (Throwable e) {
+			setError("##unjar " + e.getMessage());
+			ok = false;
+		}
+		if (jf != null) try { jf.close(); } catch (Throwable t) { }
+		return ok;
+	}
+	
+	private boolean saveUnjaredFile(JarFile jf, JarEntry je) {
+		BufferedInputStream bis = null;
+		FileOutputStream fos = null;
+		File file = null;
+		boolean ok = true;
+		try {
+			file = new File(sourceDir + je.getName());
+			if (file.exists()) {
+				if (file.length() > je.getSize()) {
+					File jarFile = new File(jf.getName());
+					String jarFileName = jarFile.getName();
+					setError("Unable to restore (" + jarFileName + "): existing file (" + file.getName() + ") is larger than the one you are trying to retore, so it may be more recent.");
+					String baseName = file.toString();
+					baseName = baseName.substring(0,baseName.indexOf("."));
+					setError("If you really want to overwrite the existing files, you must manually delete them (" + baseName + ".*) before restoring (" + jarFileName + ").");
+				}
+				return false;
+			}
+			
+			bis = new BufferedInputStream(jf.getInputStream(je));
+			fos = new FileOutputStream(file);
+			
+			byte[] buf = new byte[1000];
+			int bytesRead = 0;
+			
+			while((bytesRead = bis.read(buf)) != -1) {
+				fos.write(buf,0,bytesRead);
+			}
+		}
+		catch (Exception e) {
+			setError("##saveUnjaredFile: " + e.getMessage());
+			ok = false;
+		}
+		if (bis != null) {
+			try { bis.close(); } catch (IOException t) { }
+		}
+		if (fos != null) {
+			try { fos.close(); } catch (Exception e) {
+				setError("saveUnjaredFile " + file.toString() + ": " + e.getMessage());	
+				ok = false;
+			}				
+		}		
+		return ok;					
+	}
     
     private void setError(String s) {
 if (DEBUG) Logger.writeln(s);
