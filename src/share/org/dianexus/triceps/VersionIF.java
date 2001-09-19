5f23f688c89bf112ebbadacc5d103a814be55ed0
twhite_cvs <>
2001-09-19 00:09:03 +0000

    Enhancement - Added Perl5 regex to Node "mask" column, and regexMatch() function using InputValidator.java and Jakarta-ORO project

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -7,7 +7,7 @@ package org.dianexus.triceps;
 
 import java.lang.String;
 
-/*public*/ interface VersionIF extends DeveloperLicenseIF {
+/*public*/ interface VersionIF extends CICLicenseIF {
     /*public*/ final static boolean DEBUG = true;
     /*public*/ final static boolean AUTHORABLE = true;
     /*public*/ final static boolean DEPLOYABLE = true;
@@ -16,7 +16,7 @@ import java.lang.String;
     /*public*/ final static boolean DEMOABLE = (!AUTHORABLE && !DEPLOYABLE);
     /*public*/ final static boolean DEVELOPERABLE = (AUTHORABLE && DEPLOYABLE);
     /*public*/ final static String VERSION_MAJOR = "2.9";
-    /*public*/ final static String VERSION_MINOR = "0";
+    /*public*/ final static String VERSION_MINOR = "1";
     /*public*/ final static String VERSION_TYPE = ((DEVELOPERABLE) ? "Development System" : ((AUTHORABLE) ? "Authoring System" : ((DEPLOYABLE) ? "Interviewing System" : "Demo")));
     /*public*/ final static String VERSION_NAME = STUDY_ALIAS + " version of Triceps " + VERSION_TYPE + " version " + VERSION_MAJOR + "." + VERSION_MINOR;
     /*public*/ final static String VERBOSE_LICENSE_MSG = "This <B>" + VERSION_NAME +
