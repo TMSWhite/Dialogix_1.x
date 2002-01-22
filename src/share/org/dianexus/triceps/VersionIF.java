04eb826aa0f80569b0d3745c5f420f91a1d77192
twhite_cvs <>
2002-01-22 21:55:11 +0000

    resetting XML attribute

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -9,7 +9,7 @@ import java.lang.String;
 
 /*public*/ interface VersionIF extends DeveloperLicenseIF {
     /*public*/ final static boolean DEBUG = true;
-    /*public*/ final static boolean AUTHORABLE = true;
+    /*public*/ final static boolean AUTHORABLE = false;
     /*public*/ final static boolean DEPLOYABLE = true;
 	/*public*/ final static boolean WEB_SERVER = false;
     /*public*/ final static boolean USE_VERBOSE_LICENSE_MSG = (!WEB_SERVER && false);
@@ -27,7 +27,7 @@ import java.lang.String;
 		"]";
 	/*public*/ final static String BRIEF_LICENSE_MSG = "Dialogix " + VERSION_TYPE + " version " + VERSION_MAJOR + "." + VERSION_MINOR;
 	/*public*/ final static String LICENSE_MSG = (USE_VERBOSE_LICENSE_MSG) ? VERBOSE_LICENSE_MSG : BRIEF_LICENSE_MSG;
-	/*public*/ final static boolean XML = true;
+	/*public*/ final static boolean XML = false;
 	/*public*/ final static boolean DISPLAY_WORKING = (!WEB_SERVER);	// controls whether see working files
 	/*public*/ final static boolean DISPLAY_SPLASH = true;	// controls whether see splash screen
 }
