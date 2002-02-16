8be2de51df10eee50afc0896ebd43f5c683677fb
twhite_cvs <>
2002-02-16 19:20:14 +0000

    -- Changed SESSION_TIMEOUT from 12 hours for laptops to 20 minutes for servers       -- Don't jar error log files with data files.

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -11,12 +11,12 @@ import java.lang.String;
     /*public*/ final static boolean DEBUG = true;
     /*public*/ final static boolean AUTHORABLE = false;
     /*public*/ final static boolean DEPLOYABLE = true;
-	/*public*/ final static boolean WEB_SERVER = false;
-    /*public*/ final static boolean USE_VERBOSE_LICENSE_MSG = (!WEB_SERVER && false);
+	/*public*/ final static boolean WEB_SERVER = true;
+    /*public*/ final static boolean USE_VERBOSE_LICENSE_MSG = false;	// (!WEB_SERVER && false);
     /*public*/ final static boolean DEMOABLE = (!AUTHORABLE && !DEPLOYABLE);
     /*public*/ final static boolean DEVELOPERABLE = (AUTHORABLE && DEPLOYABLE);
     /*public*/ final static String VERSION_MAJOR = "2.9";
-    /*public*/ final static String VERSION_MINOR = "4";
+    /*public*/ final static String VERSION_MINOR = "5";
     /*public*/ final static String VERSION_TYPE = ((DEVELOPERABLE) ? "Development System" : ((AUTHORABLE) ? "Authoring System" : ((DEPLOYABLE) ? "Interviewing System" : "Demo")));
     /*public*/ final static String VERSION_NAME = STUDY_ALIAS + " version of Dialogix " + VERSION_TYPE + " version " + VERSION_MAJOR + "." + VERSION_MINOR;
     /*public*/ final static String VERBOSE_LICENSE_MSG = "This <B>" + VERSION_NAME +
@@ -30,4 +30,6 @@ import java.lang.String;
 	/*public*/ final static boolean XML = false;
 	/*public*/ final static boolean DISPLAY_WORKING = (!WEB_SERVER);	// controls whether see working files
 	/*public*/ final static boolean DISPLAY_SPLASH = true;	// controls whether see splash screen
+	/*public*/ final static boolean	SAVE_ERROR_LOG_WITH_DATA = false;	// (!WEB_SERVER);	// don't save error log file if running on server
+	/*public*/ final static int SESSION_TIMEOUT = ((WEB_SERVER) ? (60*20) : (60*60*12));	// 20 minutes for web server; 12 hours for laptop
 }
