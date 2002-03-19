196dd3e67a2f605303ed02095053f4cb0a4bfc3f
twhite_cvs <>
2002-03-19 18:01:53 +0000

    version information now compiled-in via Ant and build.properties

--- src/share/org/dianexus/triceps/VersionIF.java
+++ src/share/org/dianexus/triceps/VersionIF.java
@@ -7,16 +7,16 @@ package org.dianexus.triceps;
 
 import java.lang.String;
 
-/*public*/ interface VersionIF extends DeveloperLicenseIF {
+/*public*/ interface VersionIF extends @@DIALOGIX.LICENSE@@LicenseIF {
     /*public*/ final static boolean DEBUG = true;
-    /*public*/ final static boolean AUTHORABLE = false;
-    /*public*/ final static boolean DEPLOYABLE = true;
-	/*public*/ final static boolean WEB_SERVER = true;
+    /*public*/ final static boolean AUTHORABLE = @@DIALOGIX.AUTHORABLE@@;
+    /*public*/ final static boolean DEPLOYABLE = @@DIALOGIX.DEPLOYABLE@@;
+	/*public*/ final static boolean WEB_SERVER = @@DIALOGIX.WEB_SERVER@@;
     /*public*/ final static boolean USE_VERBOSE_LICENSE_MSG = false;	// (!WEB_SERVER && false);
     /*public*/ final static boolean DEMOABLE = (!AUTHORABLE && !DEPLOYABLE);
     /*public*/ final static boolean DEVELOPERABLE = (AUTHORABLE && DEPLOYABLE);
-    /*public*/ final static String VERSION_MAJOR = "2.9";
-    /*public*/ final static String VERSION_MINOR = "6";
+    /*public*/ final static String VERSION_MAJOR = "@@DIALOGIX.VERSION_MAJOR@@";
+    /*public*/ final static String VERSION_MINOR = "@@DIALOGIX.VERSION_MINOR@@";
     /*public*/ final static String VERSION_TYPE = ((DEVELOPERABLE) ? "Development System" : ((AUTHORABLE) ? "Authoring System" : ((DEPLOYABLE) ? "Interviewing System" : "Demo")));
     /*public*/ final static String VERSION_NAME = STUDY_ALIAS + " version of Dialogix " + VERSION_TYPE + " version " + VERSION_MAJOR + "." + VERSION_MINOR;
     /*public*/ final static String VERBOSE_LICENSE_MSG = "This <B>" + VERSION_NAME +
@@ -27,7 +27,7 @@ import java.lang.String;
 		"]";
 	/*public*/ final static String BRIEF_LICENSE_MSG = "Dialogix " + VERSION_TYPE + " version " + VERSION_MAJOR + "." + VERSION_MINOR;
 	/*public*/ final static String LICENSE_MSG = (USE_VERBOSE_LICENSE_MSG) ? VERBOSE_LICENSE_MSG : BRIEF_LICENSE_MSG;
-	/*public*/ final static boolean XML = false;
+	/*public*/ final static boolean XML = @@DIALOGIX.XML@@;
 	/*public*/ final static boolean DISPLAY_WORKING = (!WEB_SERVER);	// controls whether see working files
 	/*public*/ final static boolean DISPLAY_SPLASH = true;	// controls whether see splash screen
 	/*public*/ final static boolean	SAVE_ERROR_LOG_WITH_DATA = false;	// (!WEB_SERVER);	// don't save error log file if running on server
