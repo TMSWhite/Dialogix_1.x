778a7f883197ac8d6ee1a0a88dd13b8fa3c9f4bf
twhite_cvs <>
2002-03-27 15:01:10 +0000

    4 column HTML showing the relevance column info -- e.g. for IRB members

---
+++ perl/showlogic.pl
@@ -0,0 +1,68 @@
+#perl script to facilitate translation of Dialogix files
+#goal - create editable HTML files
+
+use strict;
+
+&main(@ARGV);
+
+sub main {
+	if ($#ARGV != 2) {
+		die "usage:  showlogic.pl source-file ouput-html-file title";
+	}
+	
+	open (SRC, "<$ARGV[0]") or die "unable to open $ARGV[0]";
+	my @lines = (<SRC>);
+	close (SRC);
+	
+	open (OUT, ">$ARGV[1]") or die "unable to write to $ARGV[1]";
+	
+
+	print OUT  qq|<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">|, "\n";
+	print OUT  qq|<HTML>|, "\n";
+	print OUT  qq|	<HEAD>|, "\n";
+	print OUT  qq|		<META HTTP-EQUIV="Content-Type" CONTENT="text/html;CHARSET=iso-8859-1">|, "\n";
+	print OUT  qq|	</HEAD>|, "\n";
+	print OUT  qq|	<TITLE>Translation File for $ARGV[2]</TITLE>|, "\n";
+	print OUT  qq|<BODY>|, "\n";
+	print OUT  qq|	<P ALIGN='center'><b>Logic File for $ARGV[2]</b></P>|, "\n";
+	print OUT  qq|	<TABLE border='1'>|, "\n";
+	print OUT  qq|		<tr><td width="5%">#</td><td width="%15">Variable Name</td><td width="15%">Relevance</td><td width='40%'>Question</td><td width='20%'>Answers</td></tr>|, "\n";
+	
+	my $count = 0;
+	
+	foreach (@lines) {
+		chomp;
+		next if (/^(RESERVED)|(COMMENT)/);
+		my @args = split(/\t/);
+		
+		++$count;
+		
+		next if ($args[4] eq 'e');	# skip eval nodes
+		
+		print OUT  qq|		<tr><td>$count</td><td>$args[1]</td><td>$args[3]</td><td>$args[6]</td><td>| . &answers($args[7]) . qq|</td></tr>|, "\n";
+	}
+
+	print OUT  qq|	</TABLE>|, "\n";
+	print OUT  qq|</BODY>|, "\n";
+	print OUT  qq|</HTML>|, "\n";
+	
+	close (OUT);
+}
+
+sub answers {
+	my $arg = shift;
+	
+	my @args = split(/\|/,$arg);
+	
+	shift(@args);
+	
+	my $str;
+	
+	while (@args) {
+		$str .= "[" . $args[0] . "] " . $args[1] . "<br />";
+		shift(@args);
+		shift(@args);
+	}
+	$str .= '&nbsp;';
+	return $str;
+}
