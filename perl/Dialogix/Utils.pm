2636ac08ecd22f36b653296c4fdf9b7df5a1909a
twhite_cvs <>
2002-04-23 18:10:56 +0000

    move %$Prefs initialization to Utils.pm pass %$Prefs to support files, rather than command line args unjar now moves files to appropriate subdirs create verbose uniqueID

--- perl/Dialogix/Utils.pm
+++ perl/Dialogix/Utils.pm
@@ -13,7 +13,7 @@ BEGIN {
     # set the version for version checking
     $VERSION     = 2.00;
 
-    @EXPORT      = qw(readPrefs,whichInstrument);
+    @EXPORT      = qw(readPrefs,whichInstrument,readDialogixPrefs,mychdir);
     %EXPORT_TAGS = ( FIELDS => [ @EXPORT_OK, @EXPORT ] );
 
     # your exported package globals go here,
@@ -82,33 +82,66 @@ sub whichInstrument($) {
 	my $count = 0;
 	my $filename = '';
 	my ($title, $version_major, $version_minor);
+	my $triceps_major = 0;
+	my $triceps_minor = 0;
 	my $type = 'DATA';	# if unlabeled, assume that it is data
 	my $timestamp = 0;
 	while (<IN>) {
 		++$count;
-		last if ($count > 50);	# don't go beyond reasonable header section length
+#		last if ($count > 50);	# don't go beyond reasonable header section length
 		my @vals = split(/\t/);
 		if ($vals[0] eq 'RESERVED') {
 			if ($vals[1] eq '__TRICEPS_FILE_TYPE__') {
-				$type = $vals[2];
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$type = $1;
+				}					
 				$filename = $datfile;
 			}					
 			elsif ($vals[1] eq '__SCHEDULE_SOURCE__') {
-				$filename = $vals[2];
-				$timestamp = $vals[3];
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$filename = $1;
+				}				
+			}
+			elsif ($vals[1] eq '__START_TIME__' && $timestamp==0) {
+				# only set timestamp once
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$timestamp = $1;
+				}				
 			}
 			elsif ($vals[1] eq '__TITLE__') {
-				$title = $vals[2];
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$title = $1;
+				}				
 			}
 			elsif ($vals[1] eq '__SCHED_VERSION_MAJOR__') {
-				$version_major = $vals[2];
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$version_major = $1;
+				}
 			}
 			elsif ($vals[1] eq '__SCHED_VERSION_MINOR__') {
-				$version_minor = $vals[2];
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$version_minor = $1;
+				}
 			}
+			elsif ($vals[1] eq '__TRICEPS_VERSION_MAJOR__') {
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$triceps_major = $1;
+				}				
+			}
+			elsif ($vals[1] eq '__TRICEPS_VERSION_MINOR__') {
+				if ($vals[2] =~ /^\s*(.*)\s*$/) {
+					$triceps_minor = $1;
+				}		
+			}			
 		}
 	}
 	close (IN);
+	
+	my	$triceps_version = "$triceps_major.$triceps_minor";
+	$triceps_version =~ s/\.\././g;	# remove adjacent periods
+	
+	my $when = &fixTime($triceps_version,$timestamp);
+	$when =~ s/[\/:\s]/_/g;
 	
 	if ($filename =~ /^.+[\\\/](.+)(\.(jar|txt))$/) {
 		$filename = $1;
@@ -118,16 +151,119 @@ sub whichInstrument($) {
 	}
 	
 	if (($filename =~ /^\s*$/) || ($title =~ /^\s*$/)) {
-		return ($UNKNOWN_INST,$UNKNOWN_INST,$timestamp);
+		return ($UNKNOWN_INST,$UNKNOWN_INST,$timestamp,$when);
 	}
 	else {
 		my $name = "$title-v$version_major.$version_minor-($filename)";
 		$name =~ s/\.\././g;
-		$name =~ s/[\\\/:\*\?\"<>\| 	]/_/g;
-		return ($filename,$name,$timestamp);
+		$name =~ s/[\\\/:\*\?\"<>\|\s]/_/g;	# remove spaces and characters disallowed in Windows folder names
+		return ($filename,$name,$timestamp,$when);
 	}
 }
 
+sub readDialogixPrefs($) {
+	#
+	# study specific variables
+	#
+	my $conf_file = shift;
+	
+	my $Defaults = {
+		JAR =>  '/jdk1.3/bin/jar',	# path to jar program (will be different on Unix)
+		JAR_FILES => '../jars/*.jar',	# path to jar files (e.g. if on a server)
+		RESULTS_DIR => '../analysis',	# path to where analyses should be put
+		UNJAR_DIR => '../unjar',			# path where unjared files should be put
+		UNFINISHED_DIR => '../working',	# path where incomplete files are stored
+		INSTRUMENT_DIR => '../instrument',	# path where source schedule / instrument stored
+		DAT_FILES => '../unjar/*.dat ../working/*',
+		EVT_FILES => '../unjar/*.evt ../working/*.evt',
+		PERL_SCRIPTS_PATH => '../perl',	# path to perl scripts - use '*' if in same directory as files
+		
+		UNIQUE_ID => '*',	#unique identifier
+		modularizeByPrefix => '*',
+		discardVarsMatchingPattern => '*',
+		NA => 99999,		# values to substitute for reserved words ('*' means do not substitute)
+		REFUSED => 55555,
+		UNKNOWN => 33333,
+		HUH => 22222,
+		INVALID => 11111,
+		UNASKED => 44444,
+		INSTRUMENT => '',	# name of the instrument file (without .txt extensions)
+		SORTBY => 'sortby_variable_name',	# 'sortby_order_asked'
+		VARNAME_FROM_COLUMN => 1,	# use '0' for concept, '1' for internalName, '2' for externalName
+		
+		VARMAP_INFO_FILE => 'varMapInfo',
+		
+		sched2sas => 0,					
+		unjarall => 0,				
+		moveWorkingFiles => 0,		
+		removeOldAnalysisFiles => 0,
+		update_dat => 0,			
+		dat2sas => 0,				
+		evt2sas => 0,				
+		# cleanup               
+		removeEvtFiles => 0,		
+		removeErrFiles => 0,		
+		removeDatFiles => 0,		
+	};
+	
+	my $Prefs = &readPrefs($Defaults,$conf_file);
 
+	# create composite variables	
+	$Prefs->{DAT2SAS} = "$Prefs->{PERL_SCRIPTS_PATH}/dat2sas.pl";
+	$Prefs->{EVT2SAS} = "$Prefs->{PERL_SCRIPTS_PATH}/evt2sas.pl";
+	$Prefs->{UPDATE_DAT} = "$Prefs->{PERL_SCRIPTS_PATH}/update_dat.pl";
+	$Prefs->{SCHED2SAS} = "$Prefs->{PERL_SCRIPTS_PATH}/sched2sas.pl";
+	$Prefs->{INSTRUMENT_FILE} = "$Prefs->{INSTRUMENT_DIR}/$Prefs->{INSTRUMENT}.txt";	
+	
+	return ($Prefs);
+}
+
+sub mychdir($) {
+	my $path = shift;
+	chdir($path) or die "unable to chdir($path)";
+}
+
+sub fixTime($$) {
+	my $triceps_version = shift;
+	my $arg = shift;
+	if ($triceps_version >= 2.5) {
+		return convertTime($arg);
+	}
+	elsif ($arg =~ /^(\d+?)\.(\d+?)\.(\d+?)\.\.(\d+?)\.(\d+?)\.(\d+?)$/) {
+		# e.g. 2000.10.26..02.20.48
+		my $ans = sprintf("%04d-%02d-%02d %02d:%02d", $1, $2, $3, $4, $5);
+		return $ans;
+	}
+	else {
+		return 'unknown';
+	}
+}
+
+sub convertTime($) {
+	my $arg = shift;
+	$arg = localtime(int($arg/1000));
+	if ($arg =~ /(\w+?)\s+(\w+?)\s+(\d+?)\s+(\d+?):(\d+?):(\d+?)\s+(\d\d\d\d)/) {
+		#Wed Jun 06 14:18:33 2001
+		my $ans = sprintf("%04d-%02d-%02d %02d:%02d", $7, &month($2), $3, $4, $5);
+		return $ans;
+	}
+}
+
+sub month($) {
+	$_ = shift;
+	return 1 if (/Jan/i);
+	return 2 if (/Feb/i);
+	return 3 if (/Mar/i);
+	return 4 if (/Apr/i);
+	return 5 if (/May/i);
+	return 6 if (/Jun/i);
+	return 7 if (/Jul/i);
+	return 8 if (/Aug/i);
+	return 9 if (/Sep/i);
+	return 10 if (/Oct/i);
+	return 11 if (/Nov/i);
+	return 12 if (/Dec/i);
+	return 0;
+}
 
 1;	# module must return a true value
--- perl/unjar.pl
+++ perl/unjar.pl
@@ -16,76 +16,21 @@ use strict;
 
 use Dialogix::Utils;
 
-#
-# study specific variables - passed to dat2sas
-#
-my $Prefs;
-$Prefs = {
-	JAR =>  '/jdk1.3/bin/jar',	# path to jar program (will be different on Unix)
-	JAR_FILES => '../jars/*.jar',	# path to jar files (e.g. if on a server)
-	RESULTS_DIR => '../analysis',	# path to where analyses should be put
-	UNJAR_DIR => '../unjar',			# path where unjared files should be put
-	UNFINISHED_DIR => '../working',	# path where incomplete files are stored
-	INSTRUMENT_DIR => '../instrument',	# path where source schedule / instrument stored
-	DAT_FILES => '../unjar/*.dat ../working/*',
-	EVT_FILES => '../unjar/*.evt ../working/*.evt',
-	PERL_SCRIPTS_PATH => '../perl',	# path to perl scripts - use '*' if in same directory as files
-	
-	UNIQUE_ID => '*',	#unique identifier
-	modularizeByPrefix => '*',
-	discardVarsMatchingPattern => '*',
-	NA => 99999,		# values to substitute for reserved words ('*' means do not substitute)
-	REFUSED => 55555,
-	UNKNOWN => 33333,
-	HUH => 22222,
-	INVALID => 11111,
-	UNASKED => 44444,
-	INSTRUMENT => '',	# name of the instrument file (without .txt extensions)
-	SORTBY => 'sortby_variable_name',	# 'sortby_order_asked'
-	VARNAME_FROM_COLUMN => 1,	# use '0' for concept, '1' for internalName, '2' for externalName
-	
-	VARMAP_INFO_FILE => 'varMapInfo',
-	
-	sched2sas => 0,					
-	unjarall => 0,				
-	moveWorkingFiles => 0,		
-	removeOldAnalysisFiles => 0,
-	update_dat => 0,			
-	dat2sas => 0,				
-	evt2sas => 0,				
-	# cleanup               
-	removeEvtFiles => 0,		
-	removeErrFiles => 0,		
-	removeDatFiles => 0,		
-
-};  
-    
-#filesystem specific variables
-#   
-my ($DAT2SAS, $EVT2SAS, $UPDATE_DAT, $SCHED2SAS, $INSTRUMENT_FILE);
-
 if ($#ARGV != 0) {
 	print "Usage:\nperl unjar.pl <config-file>\n";
 	print "see sample.conf for an example of the config parameters\n";
 	exit(0);
 }
+
+my ($Prefs,$conf_file);
     
 &main(@ARGV);
 
 sub main {
-	my $conf_file = shift;
-	$Prefs = &Dialogix::Utils::readPrefs($Prefs,$conf_file);
-	&initVars;
+	$conf_file = shift;
+	$Prefs = &Dialogix::Utils::readDialogixPrefs($conf_file);
 	return 0 unless &prepare;
 	&doall;
-}
-
-sub initVars {
-	$DAT2SAS = "$Prefs->{PERL_SCRIPTS_PATH}/dat2sas.pl";
-	$EVT2SAS = "$Prefs->{PERL_SCRIPTS_PATH}/evt2sas.pl";
-	$UPDATE_DAT = "$Prefs->{PERL_SCRIPTS_PATH}/update_dat.pl";
-	$SCHED2SAS = "$Prefs->{PERL_SCRIPTS_PATH}/sched2sas.pl";
-	$INSTRUMENT_FILE = "$Prefs->{INSTRUMENT_DIR}/$Prefs->{INSTRUMENT}.txt";	
 }
 
 sub doall {
@@ -130,11 +75,11 @@ sub checkDir {
 sub unjarall {
 	my @files = glob($Prefs->{JAR_FILES});
 	
-	chdir($Prefs->{UNJAR_DIR});
+	&Dialogix::Utils::mychdir($Prefs->{UNJAR_DIR});
 	
 	my $tmpdir = "$Prefs->{UNJAR_DIR}/../_temp_";
 	checkDir($tmpdir,0);
-	chdir($tmpdir);
+	&Dialogix::Utils::mychdir($tmpdir);
 	
 	foreach (@files) {
 		# unjar to temp directory; then determine which instrument belong to; then move to that new directory
@@ -164,10 +109,11 @@ sub unjarall {
 		&doit($msg);
 		unlink $srcname;
 	}
+	&Dialogix::Utils::mychdir($Prefs->{PERL_SCRIPTS_PATH});
 }
 
 sub moveWorkingFiles {
-	chdir($Prefs->{UNFINISHED_DIR});
+	&Dialogix::Utils::mychdir($Prefs->{UNFINISHED_DIR});
 	my @files = glob("*.dat *.");
 	
 	foreach (@files) {
@@ -191,6 +137,7 @@ sub moveWorkingFiles {
 			unlink $file;		
 		}
 	}
+	&Dialogix::Utils::mychdir($Prefs->{PERL_SCRIPTS_PATH});
 }
 
 sub moveDataFiles {
@@ -204,7 +151,7 @@ sub moveDataFiles {
 		$srcname = $_;
 	}
 	
-	my ($filename,$inst,$timestamp) = &Dialogix::Utils::whichInstrument($files[0]);	# assume that they are all from the same same data prefix
+	my ($filename,$inst,$timestamp,$when) = &Dialogix::Utils::whichInstrument($files[0]);	# assume that they are all from the same same data prefix
 	print "$files[0] => $inst\n";
 	my $locdir = $inst;
 	my $dstdir = "$Prefs->{UNJAR_DIR}/$locdir";
@@ -215,7 +162,7 @@ sub moveDataFiles {
 	$dstdir =~ s/\//\\/g;
 	
 	foreach (@files) {
-		&doit("copy \"$_\" \"$dstdir\\$timestamp-($srcname)-$_\"");
+		&doit("copy \"$_\" \"$dstdir\\$when-($srcname)-$_\"");
 	}
 	return $inst;
 }
@@ -237,20 +184,17 @@ sub removeOldAnalysisFiles {
 }
 
 sub update_dat {
-	chdir($Prefs->{UNJAR_DIR});
-	my $command = "perl $UPDATE_DAT $Prefs->{VARMAP_INFO_FILE} $Prefs->{DAT_FILES}";
+	my $command = "perl $Prefs->{UPDATE_DAT} $conf_file $Prefs->{DAT_FILES}";
 	&doit($command);
 }
 
 sub dat2sas {
-	chdir($Prefs->{RESULTS_DIR});
-	my $command = "perl $DAT2SAS $Prefs->{SORTBY} $Prefs->{INSTRUMENT_DIR}/$Prefs->{INSTRUMENT} $Prefs->{UNIQUE_ID} $Prefs->{modularizeByPrefix} $Prefs->{discardVarsMatchingPattern} $Prefs->{NA} $Prefs->{REFUSED} $Prefs->{UNKNOWN} $Prefs->{HUH} $Prefs->{INVALID} $Prefs->{UNASKED} $Prefs->{DAT_FILES}";
+	my $command = "perl $Prefs->{DAT2SAS} $conf_file";
 	&doit($command);
 }
 
 sub evt2sas {
-	chdir($Prefs->{RESULTS_DIR});
-	my $command = "perl $EVT2SAS $Prefs->{INSTRUMENT_DIR}/$Prefs->{INSTRUMENT} $Prefs->{UNIQUE_ID} $Prefs->{discardVarsMatchingPattern} $Prefs->{EVT_FILES}";
+	my $command = "perl $Prefs->{EVT2SAS} $conf_file";
 	&doit($command);
 }
 
@@ -276,7 +220,6 @@ sub removeErrFiles {
 }
 
 sub sched2sas {
-	chdir($Prefs->{INSTRUMENT_DIR});
-	my $command = "perl $SCHED2SAS $Prefs->{SORTBY} $Prefs->{VARNAME_FROM_COLUMN} $Prefs->{modularizeByPrefix} $Prefs->{discardVarsMatchingPattern} $Prefs->{RESULTS_DIR} $Prefs->{NA} $Prefs->{REFUSED} $Prefs->{UNKNOWN} $Prefs->{HUH} $Prefs->{INVALID} $Prefs->{UNASKED} $INSTRUMENT_FILE";
+	my $command = "perl $Prefs->{SCHED2SAS} $conf_file $Prefs->{INSTRUMENT_FILE}";
 	&doit($command);
 }
