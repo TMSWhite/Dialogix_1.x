b16af459c9df33b1be820026ffeb2fbf6e9e619b
tmw <>
2003-02-05 18:08:05 +0000

    use JDK1.4 by default

--- ant.bat
+++ ant.bat
@@ -1,7 +1,7 @@
 REM makefile for building Dialogix using Ant
 
 REM set JAVA_HOME=c:\j2sdk1.4.0
-set JAVA_HOME=c:\jdk1.3
+set JAVA_HOME=c:\jdk1.4
 set ANT_HOME=c:\apache\ant
 
 call %ANT_HOME%\bin\ant.bat %1 %2 %3 %4 %5
