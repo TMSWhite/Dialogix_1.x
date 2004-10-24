db2cbe5c89fca2019c112ed27262fa537fc62926
tmw <>
2004-10-24 22:39:39 +0000

    updated paths

--- ant.bat
+++ ant.bat
@@ -2,6 +2,6 @@ REM makefile for building Dialogix using Ant
 
 REM set JAVA_HOME=c:\j2sdk1.4.0
 set JAVA_HOME=c:\jdk1.4
-set ANT_HOME=c:\apache\ant
+set ANT_HOME=c:\bin\ant
 
 call %ANT_HOME%\bin\ant.bat %1 %2 %3 %4 %5
