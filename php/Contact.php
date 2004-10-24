533cb46af5556f46d3bcb4911be6f890cf0745db
tmw <>
2004-10-24 22:33:05 +0000

    PHP code to show collaborations and instruments on psychinformatics.nsypi.org/Dialogix/

---
+++ php/Contact.php
@@ -0,0 +1,49 @@
+<?php include("Dialogix_Table_PartA.php"); ?>
+
+<div style="text-align: center;"><br>
+<img
+src="http://psychinformatics.nyspi.org:8080/images/dialogoSmall.jpg"
+align="bottom" border="0" height="40" width="89"> <br>
+<font size="5">a System for Rapidly </font><i><font size="5">Developing</font></i><font
+size="5">, </font><i><font size="5">Deploying</font></i><font size="5">,
+and </font><i><font size="5">Analyzing</font></i><font size="5">
+Research Studies</font> <br>
+<br>
+Thomas M. White, MD, MS, MA <br>
+Director, Unit of Psychiatric Informatics<br>
+Center for Information Technology and Evaluation Research<br>
+New York State Office of Mental Health<br>
+330 Fifth Ave, 9th Floor<br>
+212-330-6358<br>
+<a href="mailto:coevtmw@omh.state.ny.us">coevtmw@omh.state.ny.us</a><br>
+<a href="http://www.tomwhitemd.com/">Homepage</a>
+</div>
+<p>
+<p>
+<table border="1" cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+<td width="33%"><img
+src="http://psychinformatics.nyspi.org:8080/images/nypi.jpg"
+alt="New York State Psychiatric Institute" align="bottom" border="0"
+height="62" width="300"></td>
+<td align="center" width="33%">
+<p align="center"><img
+src="http://psychinformatics.nyspi.org:8080/images/omh_logo.jpg"
+alt="OMH Logo" align="bottom" border="0" height="91" width="170">
+</p>
+</td>
+<td width="33%">
+<div align="right">
+<p><img
+src="http://psychinformatics.nyspi.org:8080/images/cpmc_dmi.jpg"
+alt="Columbia Department of Medical Informatics" align="bottom"
+border="0" height="91" width="300"> </p>
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+
+
+<?php include("Dialogix_Table_PartB.php"); ?>
---
+++ php/Dialogix_Table_PartA.php
@@ -0,0 +1,22 @@
+<!-- Start of Table -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+<title>Dialogix Home</title>
+</head>
+<body>
+<table style="height: 100%; text-align: left; width: 100%;" border="0" cellpadding="2" cellspacing="2">
+<tr>
+<td style="width: 100px; vertical-align: top;">
+ <a href="http://psychinformatics.nyspi.org/Dialogix/index.php">
+<img src="http://psychinformatics.nyspi.org:8080/images/dialogoSmall.jpg" align="bottom" border="0" height="40" width="89">
+</a>
+<br>
+<br>
+ <a href="http://psychinformatics.nyspi.org/Dialogix/Collaborations.php">Collaborations</a><br>
+ <a href="http://psychinformatics.nyspi.org/Dialogix/Instruments.php">Instruments</a><br>
+ <a href="http://psychinformatics.nyspi.org/Dialogix/Publications.php">Publications</a><br>
+ <a href="http://psychinformatics.nyspi.org/Dialogix/Contact.php">Contact</a><br>
+</td>
+<td style="vertical-align: top;">
