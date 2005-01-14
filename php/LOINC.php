a8513a14bbe9d70c7f5c547c80c3924fbf22323b
tmw <>
2005-01-14 03:39:46 +0000

    updates

--- php/LOINC.php
+++ php/LOINC.php
@@ -3,5 +3,11 @@
 <?php include("Dialogix_Table_PartA.php"); ?>
 
 <li><a href="UniqueLOINCItems.php">Unique LOINC Items</a>
+<li><a href="DialogixOverview-2004-12-16.ppt">Dialogix Overview</a>
+<li><a href="Proposal for AHRQ funding for Dialogix.doc">AHRQ proposal draft -- 11/22/2004</a>
+<hr>
+
+<?php include("LOINCplan.htm"); ?>
+
 
 <?php include("Dialogix_Table_PartB.php"); ?>
