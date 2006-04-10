fa497c991ca28eb57ce26624f72db61025f1d9c0
istcgxl <>
2006-04-10 19:35:27 +0000

    ant bat edited path to ant for Linux Server

--- ant.bat
+++ ant.bat
@@ -1,5 +1,5 @@
 REM makefile for building Dialogix using Ant
 
-set ANT_HOME=c:\bin\ant
+set ANT_HOME=/usr/local/ant/
 
-call %ANT_HOME%\bin\ant.bat %1 %2 %3 %4 %5
+call %ANT_HOME%/bin/ant.bat %1 %2 %3 %4 %5
