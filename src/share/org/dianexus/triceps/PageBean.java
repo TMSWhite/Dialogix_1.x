aa70f8671a683dd7e5077957f6a538d6d9702a7a
istcgxl <>
2006-08-31 18:00:54 +0000

    initial Commit

---
+++ src/share/org/dianexus/triceps/PageBean.java
@@ -0,0 +1,16 @@
+package org.dianexus.triceps;
+
+public class PageBean {
+	
+ private String directive;
+
+public String getDirective() {
+	return directive;
+}
+
+public void setDirective(String directive) {
+	this.directive = directive;
+}
+ 
+ 
+}
