72e2db098a9f3e68ac7299a5ea1ccea112bd831e
istcgxl <>
2006-09-01 19:13:01 +0000

    initial commit for sandbox classes

---
+++ src/share/org/dianexus/triceps/SandBoxItem.java
@@ -0,0 +1,37 @@
+package org.dianexus.triceps;
+
+public class SandBoxItem {
+	
+	private int id;
+	private int sandbox_id;
+	private int instrument_id;
+	private int instrument_version_id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getInstrument_id() {
+		return instrument_id;
+	}
+	public void setInstrument_id(int instrument_id) {
+		this.instrument_id = instrument_id;
+	}
+	public int getInstrument_version_id() {
+		return instrument_version_id;
+	}
+	public void setInstrument_version_id(int instrument_version_id) {
+		this.instrument_version_id = instrument_version_id;
+	}
+	public int getSandbox_id() {
+		return sandbox_id;
+	}
+	public void setSandbox_id(int sandbox_id) {
+		this.sandbox_id = sandbox_id;
+	}
+	
+	
+
+}
---
+++ src/share/org/dianexus/triceps/SandBoxUserBean.java
@@ -0,0 +1,33 @@
+package org.dianexus.triceps;
+
+public class SandBoxUserBean {
+	private int id;
+	private int user_id;
+	private int sandbox_id;
+	private int role_id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+	public int getSandbox_id() {
+		return sandbox_id;
+	}
+	public void setSandbox_id(int sandbox_id) {
+		this.sandbox_id = sandbox_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+}
